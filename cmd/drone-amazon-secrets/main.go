1359c1e3a73d8a00ad35eaf5ead1ed2edd72b8bc
bmosz <wojciech.szychta@persgroep.net>
2020-05-19 11:45:57 +0200

    Change plugin path

--- cmd/drone-amazon-secrets/main.go
+++ cmd/drone-amazon-secrets/main.go
@@ -8,8 +8,8 @@ import (
 	"net/http"
 	"os"
 
-	"github.com/drone/drone-amazon-secrets/plugin"
-	"github.com/drone/drone-amazon-secrets/server"
+	"github.com/BerlingskeMedia/drone-amazon-secrets/plugin"
+	"github.com/BerlingskeMedia/drone-amazon-secrets/server"
 	"github.com/drone/drone-go/plugin/secret"
 
 	"github.com/aws/aws-sdk-go-v2/aws/ec2metadata"
